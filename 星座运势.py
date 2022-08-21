7bd048ec925ca3646bfa78ab4b784e1d982b8acc
wanxiao666 <215293160@qq.com>
2022-08-21 21:01:20 +0800

    加入了多个输出

---
+++ 星座运势.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+xingzuo = "Cancer"
+yunshia= "https://www.d1xz.net/yunshi/today/Aries/"
+yunshiw= yunshia[:34] + xingzuo
+req = requests.get(url=yunshiw)
+req.encoding = "utf-8"
+html = req.text
+soup = BeautifulSoup(req.text, features="html.parser")
+company_items = soup.find_all("div", class_="txt")
+for company_item in company_items:
+    dd = company_item.text.strip()
+    print(dd)
+
+
+
