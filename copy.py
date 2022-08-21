ce0517f1f3cda86d4143ec428e147f8d9926391f
monkeyiii <2629104216@qq.com>
2022-08-21 22:19:59 +0800

    class new funtion

---
+++ copy.py
@@ -0,0 +1,29 @@
+import datetime
+
+i = datetime.datetime.now()
+
+
+class birthday:
+    def __init__(self, year, month, day):#构造方法
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def say_birthday(self):
+        print(self.year,self.month,self.day)
+
+    def inquiry(self):   # 查询离下个生日差多远
+        a = 1
+
+    def constellation(self):   # 查询星座
+        c = 1
+
+    def luck(self):   # 查询运势
+        b = 1
+
+
+s1 = birthday(2002, 4, 30)
+a = s1.year
+print(a)
+s1.say_birthday()
+
